08be7863d459b252bb5f792b1b9d648b33de1bc0
Blaine Lewis <blaine.lewis.1@outlook.com>
2020-04-27 12:37:30 -0400

    Updated all dependencies, moved to yarn, added new config for webpack to eject

---
+++ src/options.js
@@ -0,0 +1,7 @@
+/*global chrome*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+ReactDOM.render(<div> hello world </div>, document.getElementById("root"));
